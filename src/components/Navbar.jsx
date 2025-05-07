4953567befed2a47d21827d0977ecc3c2aea46c2
Antorou <rousselotantoine7@gmail.com>
2025-05-07 12:57:39 +0200

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import { NavLink } from 'react-router-dom';
-import '../assets/Navbar.css'; // Make sure this path matches your folder structure
+import '../assets/Navbar.css';
+import logo from '../assets/AR.png'; // Adjust the path to your logo
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Antoine Rousselot</div>
+      <div className="navbar-brand">
+        <NavLink to="/">
+          <img src={logo} alt="AR Logo" />
+        </NavLink>
+      </div>
       <div className="navbar-links">
         <NavLink to="/" end>Home</NavLink>
         <NavLink to="/about">About</NavLink>
